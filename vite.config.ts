56e72c849f1f84313228508b19f8f215e01510e9
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2025-07-28 16:42:07 +0530

    final chnages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-<<<<<<< HEAD
-=======
-import path from "path";
->>>>>>> 3c08b929f5a458570a719f4b5eccee16e2fc4c1d
 
 // https://vitejs.dev/config/
 export default defineConfig({
